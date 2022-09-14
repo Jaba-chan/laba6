d5904d0b3bcb5adbfa0f949850b6a6ed57049a88
kuzak <kuzakovevgen@gmail.com>
2022-09-14 16:59:25 +0700

    Добавьте файлы проекта.

---
+++ laba6/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int selected_ind = listBox1.SelectedIndex;
+            string str = listBox1.Items[selected_ind].ToString();
+            label1.Text = str.Replace("А", "*");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Insert(0, "АВОВА");
+            listBox1.Items.Insert(0, "АВОВА");
+        }
+
+    }
+}
